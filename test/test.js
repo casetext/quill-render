casetext/quill-render

commit d14df3550360bfa69377bfe796ac70008d0dbc88

Fix line-level styles
Don't accidentally break out of a group when a line has many inline styles

index.js
@@ -121,7 +121,7 @@ function convert(ops) {
 			} else {
 
 				for (var j = 0; j < lines.length; j++) {
-					if (group && ++group.distance >= 2) {
+					if (j > 0 && group && ++group.distance >= 2) {
 						group = null;
 					}
 					applyStyles(op.attributes, ops[i+1] && ops[i+1].attributes);

commit 1ceca240ec551dfdf17593fff91cfdd3ca9a97b7

oops, broke it
d14df3550360bfa69377bfe796ac70008d0dbc88 broke this

test/test.js
@@ -55,7 +55,7 @@ describe('quill-render', function() {
 
 	});
 
-	it('renders lists correctly', function() {
+	it('renders lists with inline formats correctly', function() {
 
 		expect(render([
 			{
@@ -90,6 +90,69 @@ describe('quill-render', function() {
 			}
 		]))
 		.to.equal('<ol><li><i>Glenn v. Brumby</i>, 663 F.3d 1312 (11th Cir. 2011)</li><li><i>Barnes v. City of Cincinnati</i>, 401 F.3d 729 (6th Cir. 2005)</li></ol><p></p>');
+
+	});
+
+
+	it('renders adjacent lists correctly', function() {
+
+		expect(render([
+			{
+				"insert": "Item 1"
+			},
+			{
+				"insert": "\n",
+				"attributes": {
+					"list": true
+				}
+			},
+			{
+				"insert": "Item 2"
+			},
+			{
+				"insert": "\n",
+				"attributes": {
+					"list": true
+				}
+			},
+			{
+				"insert": "Item 3"
+			},
+			{
+				"insert": "\n",
+				"attributes": {
+					"list": true
+				}
+			},
+			{
+				"insert": "Intervening paragraph\nItem 4"
+			},
+			{
+				"insert": "\n",
+				"attributes": {
+					"list": true
+				}
+			},
+			{
+				"insert": "Item 5"
+			},
+			{
+				"insert": "\n",
+				"attributes": {
+					"list": true
+				}
+			},
+			{
+				"insert": "Item 6"
+			},
+			{
+				"insert": "\n",
+				"attributes": {
+					"list": true
+				}
+			}
+		]))
+		.to.equal('<ol><li>Item 1</li><li>Item 2</li><li>Item 3</li></ol><p>Intervening paragraph</p><ol><li>Item 4</li><li>Item 5</li><li>Item 6</li></ol><p></p>');
 
 	});
 